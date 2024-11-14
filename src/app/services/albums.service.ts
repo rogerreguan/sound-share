b90e798d484db1df878cb4331ef28ac6e1eed663
MVRT <alj.martinez.j@gmail.com>
2024-11-14 18:25:03 +0100

    devA + devR combined

---
+++ src/app/services/albums.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { IAlbum } from 'src/model/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlbumsService {
+
+  public albums: IAlbum[] = [];
+
+  constructor() { }
+
+  addAlbum(album: IAlbum): boolean {
+    this.albums.push(album);
+    localStorage.setItem('albums', JSON.stringify(this.albums));
+    return true;
+  }
+
+  getAlbumById(id: string) {
+    return null;
+  }
+
+  getAlbums() {
+    if (localStorage.getItem('albums') == undefined) {
+      return this.albums;
+    } else {
+      this.albums = JSON.parse(localStorage.getItem('albums')!);
+      return this.albums;
+    }
+    
+  }
+
+  removeAlbum(id: string) {
+    return null
+  }
+}

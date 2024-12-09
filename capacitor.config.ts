18a62bc3ee0330cf59204f9f38493fe87fad3dcc
MVRT <alj.martinez.j@gmail.com>
2024-12-09 18:02:39 +0100

    android changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.soundshare.app',
+  appId: 'io.ionic.starter',
   appName: 'soundshare',
   webDir: 'www'
 };
